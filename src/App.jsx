8518df830760375abfa0eb3d1e890b82eae715dd
salik beigh <beighsalik51@gmail.com>
2025-09-04 14:54:14 +0530

    basic stuff done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Login from "./Login";
+import Profile from "./Profile";
+import Body from "./Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <div>hello world</div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
