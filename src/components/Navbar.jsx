dd777c63bf7058d0728b49f05110dca7208b8545
salik beigh <beighsalik51@gmail.com>
2025-09-29 14:17:22 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
           <Link to="/" className="btn btn-ghost text-xl">
-            UniPulse 💗
+            💗
           </Link>
         </div>
         <div className="flex gap-2">
